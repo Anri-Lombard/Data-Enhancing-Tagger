95476557635c8bd03347cef8a26e50a31170b172
Anri Lombard <anri.m.lombard@gmail.com>
2022-06-25 05:17:13 +0200

    Data loaded and tag template v1

---
+++ client/src/components/edit.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+ 
+export default function Edit() {
+  const PORT = 2000;
+  
+  const [form, setForm] = useState({
+    date: "",
+    description: "",
+  });
+  const params = useParams();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id.toString();
+      const response = await fetch(`http://localhost:${PORT}/tag/${id}`);
+  
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+  
+      const record = await response.json();
+      if (!record) {
+        window.alert(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+  
+      setForm(record);
+    }
+  
+    fetchData();
+  
+    return;
+  }, [params.id, navigate]);
+  
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+  
+  async function onSubmit(e) {
+    e.preventDefault();
+    const editedTag = {
+      date: form.date,
+      description: form.description,
+    };
+  
+    // This will send a post request to update the data in the database.
+    await fetch(`http://localhost:${PORT}/update/${params.id}`, {
+      method: "POST",
+      body: JSON.stringify(editedTag),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+  
+    navigate("/");
+  }
+  
+  // This following section will display the form that takes input from the user to update the data.
+  return (
+    <div>
+      <h3>Update Tag</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Date: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="date"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="tagOptions"
+              id="tagOne"
+              value="take-away"
+              checked={form.level === "take-away"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="tagOne" className="form-check-label">Take Away</label>
+          </div>
+        </div>
+        <br />
+  
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Update Tag"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ server/routes/tag.js
@@ -0,0 +1,76 @@
+const express = require("express");
+ 
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const tagRoutes = express.Router();
+ 
+// This will help us connect to the database
+const dbo = require("../db/conn");
+ 
+// This help convert the id from string to ObjectId for the _id.
+// const ObjectId = require("mongodb").ObjectId;
+ 
+ 
+// This section will help you get a list of all the records.
+tagRoutes.route("/tag").get(function (req, res) {
+ let db_connect = dbo.getDb("bank-statements");
+ db_connect
+   .collection("transactions")
+   .find({})
+   .toArray(function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+// This section will help you get a single record by id
+tagRoutes.route("/tag/:id").get(function (req, res) {
+ let db_connect = dbo.getDb();
+ let myquery = { id: req.params.id };
+ db_connect
+     .collection("transactions")
+     .findOne(myquery, function (err, result) {
+       if (err) throw err;
+       res.json(result);
+     });
+});
+ 
+// This section will help you create a new record.
+// recordRoutes.route("/record/add").post(function (req, response) {
+//  let db_connect = dbo.getDb();
+//  let myobj = {
+//    name: req.body.name,
+//    position: req.body.position,
+//    level: req.body.level,
+//  };
+//  db_connect.collection("records").insertOne(myobj, function (err, res) {
+//    if (err) throw err;
+//    response.json(res);
+//  });
+// });
+ 
+// This section will help you update a record by id.
+tagRoutes.route("/update/:id").post(function (req, response) {
+ let db_connect = dbo.getDb(); 
+ let myquery = { id: req.params.id }; 
+ let newvalues = {   
+   $set: {     
+     ...req.params,
+     category: req.params.category 
+   }, 
+  }
+});
+ 
+// This section will help you delete a record
+// recordRoutes.route("/:id").delete((req, response) => {
+//  let db_connect = dbo.getDb();
+//  let myquery = { _id: ObjectId( req.params.id )};
+//  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+//    if (err) throw err;
+//    console.log("1 document deleted");
+//    response.json(obj);
+//  });
+// });
+ 
+module.exports = tagRoutes;
