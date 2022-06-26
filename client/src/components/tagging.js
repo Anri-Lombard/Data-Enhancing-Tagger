c4db958c5df8b81607d0d1f5d551878bac50ae2a
RacquelNinaDennison <racquelnina@gmail.com>
2022-06-26 15:12:25 +0200

    Adjustments to text

--- client/src/components/tagging.js
+++ client/src/components/tagging.js
@@ -127,7 +127,7 @@ export default function Tagging() {
  // This following section will display the table with the tags the user hasn't tagged.
  return (
    <div>
-     <h3>Manual Tagger</h3>
+     <h3>Manual Tagger Styling </h3>
     {getSingleTag()}
    </div>
  );
