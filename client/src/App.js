f4fecba15ba1868e98a4ca091fff33f63663107e
Anri Lombard <anri.m.lombard@gmail.com>
2022-06-27 04:30:30 +0200

    Login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 // import logo from './logo.svg';
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Tagging from './components/tagging'
+import Login from './components/login'
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Tagging />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +20,6 @@ function App() {
         </p>
       </header> */}
 
-      <Tagging />
 
       {/* <Routes>
         <Route exact path="/" element={<TagList />} />
---
+++ client/src/components/login.js
@@ -0,0 +1,4 @@
+import React, { useEffect, useState } from "react";
+
+
+export default function Login() {}
