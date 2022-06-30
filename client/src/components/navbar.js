f39c9c3bca3cbd66634c5bb0292df26776ee3efe
Anri Lombard <anri.m.lombard@gmail.com>
2022-06-30 09:30:03 +0200

    Add logout

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,11 +12,14 @@ const NavBar = () => {
       <RB.Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
         <RB.Container>
           <RB.Navbar.Brand>Manual Tagger</RB.Navbar.Brand>
+          <Link id="linkToLogout" to="/logout">Logout</Link>
+          <RB.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <RB.Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            <RB.Navbar.Text>Signed in as: NameOfUser</RB.Navbar.Text>
+          </RB.Navbar.Collapse>
         </RB.Container>
         {/* <img src='../images/tag.png' alt="tag logo" /> */}
         {/* <h1>Manual Tagger</h1>
-        <RB.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <RB.Navbar.Collapse id="responsive-navbar-nav">
           <RB.Nav className="mr-auto">
             <Link to="/logout">
               <RB.Nav.Link href="#logout">logout</RB.Nav.Link>
