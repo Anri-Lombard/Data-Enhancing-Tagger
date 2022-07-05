60d5745a25bcb7b37cb671862f9e22fb11cde449
RacquelNinaDennison <racquelnina@gmail.com>
2022-07-05 21:44:22 +0200

    Changed the style of the text

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -11,11 +11,11 @@ const NavBar = ({ name, user }) => {
     <div className="App">
       <RB.Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
         <RB.Container>
-          <RB.Navbar.Brand>Manual Tagger</RB.Navbar.Brand>
-          <Link id="linkToLogout" to="/logout">Logout</Link>
+          <RB.Navbar.Brand className="textStyle">Manual Tagger</RB.Navbar.Brand>
+          <Link id="linkToLogout" to="/logout" className="textStyle" >Logout</Link>
           <RB.Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <RB.Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-            <RB.Navbar.Text>Signed in as: {name}</RB.Navbar.Text>
+            <RB.Navbar.Text className="textStyle">Signed in as: {name}</RB.Navbar.Text>
           </RB.Navbar.Collapse>
         </RB.Container>
       </RB.Navbar>
