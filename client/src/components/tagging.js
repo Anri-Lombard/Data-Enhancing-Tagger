fc23bbb1bf4b6765570d2aec60c716d06719c400
RacquelNinaDennison <racquelnina@gmail.com>
2022-07-06 10:54:35 +0200

    somethings broken but I will fix it

--- client/src/components/tagging.js
+++ client/src/components/tagging.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import NavBar from './navbar.js';
 import Footer from './footer.js';
 import '../css/tagging.css'
-import './jsonFilesfortags/tagInfo.js'
+import Information from './jsonFilesfortags/tagInfo';
 
 const PORT = 2000;
 
@@ -52,32 +52,11 @@ export default function Tagging({ name, user }) {
   //        -- 2 options: "tagged" = true once chosen
   // Logic of tagging the tags :) -- The above description is the idea of what we want to achieve :) 
 
-  //functons for the search bar 
+
+  //---------------------------------------------------------creating the search bar fucntionaluty-----------------------
 
 
 
-
-    // the value of the search field '=
-    const [name1, setName] = useState('');
-
-    // the search result
-    const [foundUsers, setFoundUsers] = useState(USERS);
-  
-    const filter = (e) => {
-    const keyword = e.target.value;
-    if (keyword !== '') {
-      const results = USERS.filter((user) => {
-        return user.name.toLowerCase().startsWith(keyword.toLowerCase());
-        // Use the toLowerCase() method to make it case-insensitive
-      });
-      setFoundUsers(results);
-    } else {
-      setFoundUsers(USERS);
-      // If the text field is empty, show all users
-    }
-
-    setName(keyword);
-  };
     const Tag = (props) => (
     <div>
       <div>
@@ -93,6 +72,20 @@ export default function Tagging({ name, user }) {
       </div>
     </div>
   );
+
+  const items = Information.map(data=>{
+    return(
+    <div>
+      <ul>
+        <li style={{position:'relative',left:'10vh'}}>
+          <span className="styleInfo">{data.name}</span>
+          <span className="styleInfo">{data.age}</span>
+          <span className="styleInfo">{data.country}</span>
+        </li>
+      </ul>
+    </div>
+    )
+  })
 
 
   // Synchronous
@@ -235,30 +228,10 @@ export default function Tagging({ name, user }) {
       <NavBar name={name} />
       <div className="header">
         <h3>Tag Data With Following Details:</h3>
+        {items}
         {getSingleTag()}
-        <div className="container">
-      <input
-        type="search"
-        value={name}
-        onChange={filter}
-        className="input"
-        placeholder="Filter"
-      />
-
-      <div className="user-list">
-        {foundUsers && foundUsers.length > 0 ? (
-          foundUsers.map((user) => (
-            <li key={user.id} className="user">
-              <span className="user-name1">{user.name}</span>
-            </li>
-          ))
-        ) : (
-          <h1>No results found!</h1>
-        )}
+        
       </div>
-    </div>
-      </div>
-      <Footer />
     </>
   );
 }
