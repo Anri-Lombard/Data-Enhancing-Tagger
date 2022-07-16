e1fbd4afff9af4607ef8ecbd4dc84c062be5b94e
Anri Lombard <anri.m.lombard@gmail.com>
2022-07-16 15:58:14 +0200

    Handle unfound data

--- client/src/components/tag.js
+++ client/src/components/tag.js
@@ -6,10 +6,16 @@ const Tag = React.memo((props) => {
         <div className="description-box">
             <h3 className="description-box-heading">Details</h3>
             <div className="description-box-text">
-                <p>Description: {props.tag.description}</p>
-                <p>Current Category: {props.tag.category}</p>
-                <p>Number of Users Who Tagged: {props.tag.usersTagged === undefined || props.tag.usersTagged === null ? "No One" : props.tag.usersTagged.length}</p>
-                <p>User Categories: {props.tag.userCategories === undefined || props.tag.userCategories === null ? "No Categories" : props.tag.userCategories}</p>
+                {
+                    props.tag === null ? 
+                    <p>No data left to tag</p> :
+                    <>
+                        <p>Description: {props.tag.description}</p>
+                        <p>Current Category: {props.tag.category}</p>
+                        <p>Number of Users Who Tagged: {props.tag.usersTagged === undefined || props.tag.usersTagged === null ? "No One" : props.tag.usersTagged.length}</p>
+                        <p>User Categories: {props.tag.userCategories === undefined || props.tag.userCategories === null ? "No Categories" : props.tag.userCategories}</p>
+                    </>
+                }
             </div>
         </div>
     )
--- client/src/components/tagging.js
+++ client/src/components/tagging.js
@@ -16,27 +16,33 @@ const Tagging = React.memo(({ name, user }) => {
     dispatch('filter', "");
   }, [])
 
-  useEffect(() => {
-    async function getOneTag() {
-      const response = await fetch(`http://localhost:${PORT}/tag/one`);
+  // TODO: fix continuous run
+  async function getOneTag() {
+    const response = await fetch(`http://localhost:${PORT}/tag/one`);
 
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
-
-      const tag = await response.json();
-
-      dispatch('setTagToUpdate', tag)
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
     }
 
+    const tag = await response.json();
+
+    dispatch('setTagToUpdate', tag)
+  }
+
+  useEffect(() => {
+    console.log(state.tagToUpdate);
+
     getOneTag();
-  }, [state.tagToUpdate]);
+  }, []);
 
 
   const filter = (e) => {
     const keyword = e.target.value;
+
+    getOneTag();
+    console.log(state.tagToUpdate)
 
     dispatch('filter', keyword);
   };
@@ -44,13 +50,18 @@ const Tagging = React.memo(({ name, user }) => {
 
   async function onSubmitHandler(e) {
     e.preventDefault();
+    console.log("Hi");
+
+    // get tag
+    getOneTag();
+    // update tag
+    getOneTag();
 
     let dataTagged = false;
     let userCategoriesArray = [];
     let usersTaggedArray = [];
     let editedTag = [];
     let updatedChosenCategory = "";
-    let updatedTagOptions = "";
 
     // userCategories
     if (state.tagToUpdate.userCategories === undefined) {
@@ -74,7 +85,6 @@ const Tagging = React.memo(({ name, user }) => {
     } else if (userCategoriesArray.length === 2) {
       if (userCategoriesArray[0] !== userCategoriesArray[1]) {
         // Decision state
-        updatedTagOptions = [userCategoriesArray[0], userCategoriesArray[1]]
         updatedChosenCategory = userCategoriesArray[1];
         // setChosenCategory(userCategoriesArray[1]);
       } else {
@@ -109,6 +119,9 @@ const Tagging = React.memo(({ name, user }) => {
     // dispatch('filter', "")
 
     document.getElementById("tagBtn").disabled = true;
+
+    // getOneTag();
+    // console.log(state.tagToUpdate)
   }
 
   function onChangeHandler(e) {
